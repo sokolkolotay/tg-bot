4cc0fba50d7512d771c275a7e1df57b11663930b
kolotayilya <ilha.kolotai@yandex.ru>
2026-05-27 13:05:03 +0700

    fix: added error handling

--- src/main/kotlin/com/example/service/HttpTaskService.kt
+++ src/main/kotlin/com/example/service/HttpTaskService.kt
@@ -4,18 +4,22 @@ import com.example.model.Task
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 
 class HttpTaskService(
     private val client: HttpClient,
     private val baseUrl: String
 ) : TaskService {
 
-    override suspend fun getTasks(
-        chatId: Long
-    ): List<Task> {
-
-        return client.get("$baseUrl/tasks") {
+    override suspend fun getTasks(chatId: Long): List<Task> {
+        val response = client.get("$baseUrl/tasks") {
             parameter("chatId", chatId)
-        }.body()
+        }
+
+        return if (response.status == HttpStatusCode.OK) {
+            response.body()
+        } else {
+            emptyList()
+        }
     }
 }
